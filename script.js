2281839a67a53e6bf0b01190f728d1ed43362f47
nogawanogawa <t_tsuno_tsuno@yahoo.co.jp>
2020-07-02 23:33:25 +0900

    [fix] 複雑な表記のお試し

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 let dom = document.querySelector("#root");
-let element = React.createElement("p", {}, "hello React!");
+let element = React.createElement(
+    "div", {}, [
+        React.createElement(
+            "h2", {}, "Hello!"
+        ),
+        React.createElement(
+            "h3", {}, "React Sample page."
+        ),
+        React.createElement(
+            "ul", {}, [
+                React.createElement(
+                    "li", {}, "First item"
+                ),
+                React.createElement(
+                    "li", {}, "Second item"
+                ),
+                React.createElement(
+                    "li", {}, "Third item"
+                ),
+            ]
+        ),
+    ]);
 ReactDOM.render(element, dom);
