ee2975533908da713b9c86d1e9843ba7fbb87a0e
nogawanogawa <t_tsuno_tsuno@yahoo.co.jp>
2020-07-04 20:48:21 +0900

    [fix] Stateで表示まで変える

--- react_sample/src/App.js
+++ react_sample/src/App.js
@@ -3,22 +3,31 @@ import Rect from "./Rect";
 import './App.css';
 
 class App extends Component {
-  msgstyle = {
+  msgstyle1 = {
     fontSize:"24pt",
     color:"#900",
     margin : "20px 20px",
     padding: "5px",
     borderBottom: "2px solid #900"
   }
+  msgstyle2 = {
+    fontSize:"20pt",
+    color:"white",
+    backgroundColor: "#900",
+    margin : "20px 0px",
+    padding: "5px",
+    borderBottom: "2px solid #900"
+  }
   btnstyle = {
-    fontSize: "20pt",
+    fontSize: "16pt",
     padding: "0px 10px"
   }
   constructor(props){
     super(props);
     this.state = {
       counter : 0,
-      msg : "connt start"
+      msg : "connt start",
+      flg : true
     };
     this.doAction = this.doAction.bind(this)
   }
@@ -26,14 +35,19 @@ class App extends Component {
   doAction(e) {
     this.setState((state) => ({
       counter: state.counter + 1,
-      msg: "count: " + state.counter 
+      msg: "count: " + state.counter,
+      flg: !state.flg 
     }))
   }
 
   render(){
     return <div>
       <h1>React</h1>
-      <p style={this.msgstyle}>{this.state.msg}</p>
+      {this.state.flg ? 
+        <p style={this.msgstyle1}>{this.state.msg}</p>
+      :
+        <p style={this.msgstyle2}>{this.state.msg}</p>
+      }
       <button style={this.btnstyle} onClick={this.doAction}>Click</button>
     </div>;
   }
