1e8ee007f773d00f88ff668dea819ca12fd28720
nogawanogawa <t_tsuno_tsuno@yahoo.co.jp>
2020-07-04 15:10:32 +0900

    [fix]複数コンポーネントの使用

--- react_sample/src/App.js
+++ react_sample/src/App.js
@@ -2,16 +2,31 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
-    super()
-    this.title = props.title;
-    this.message = props.message
-  }
   render(){
     return <div>
-      <h1>{this.title}</h1>
-      <p>{this.message}</p>
+      <h1>React</h1>
+      <Rect x="50" y="50" w="150" h="150" color="cyan"></Rect>
+      <Rect x="150" y="100" w="150" h="150" color="magenta"></Rect>
+      <Rect x="100" y="150" w="150" h="150" color="black"></Rect>
     </div>;
+  }
+}
+
+class Rect extends Component{
+  style = {};
+  constructor(props){
+    super(props)
+    this.style = {
+      backgroundColor : props.color,
+      position : "absolute",
+      left : props.x + "px",
+      top : props.y + "px",
+      width : props.w + "px",
+      height : props.h + "px"
+    }
+  }
+  render(){
+    return <div style={this.style}></div>
   }
 }
 
