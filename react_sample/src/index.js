8c0997e0c51ffb193a4fa5adf79ae0ca31b18069
nogawanogawa <t_tsuno_tsuno@yahoo.co.jp>
2020-07-05 16:42:07 +0900

    [add] memo app (なんかうまくいってないっぽい)

--- react_sample/src/index.js
+++ react_sample/src/index.js
@@ -1,45 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, combineReducers} from "redux"
 import {Provider} from 'react-redux'
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-//State
-let state_value ={
-    counter: 0,
-    message: "COUNTER"
-}
-
-//Reducer
-function counter(state = state_value, action){
-    switch (action.type){
-        case "INCREMENT":
-            return{
-                counter: state.counter + 1,
-                message: "INCREMENT"
-            }
-        case "DECREMENT":
-            return{
-                counter: state.counter - 1,
-                message: "DECREMENT"
-            }
-        default:
-            return state
-    }
-}
-
-let store = createStore(counter)
+import MemoStore from "./memo/Store"
 
 ReactDOM.render(
-    <Provider store={store}>
+    <Provider store={MemoStore}>
         <App />
     </Provider>,
     document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
